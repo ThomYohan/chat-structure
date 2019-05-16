ebd9f994f6c05872829004ffd3ea064dda1c8540
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-05-16 15:58:29 -0700

    finished messaging, confirm functionality soon after the rest of the app is built

---
+++ src/Components/Messages/Messages.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+
+const Messages = (props) => {
+
+    const { messages, user, typingUsers } = props
+
+
+    scrollDown() {
+        const { container } = this.refs
+        container.scrollTop = container.scrollHeight
+    }
+
+    useEffect(() => {
+        scrollDOwn()
+    })
+
+
+
+
+
+    return (
+        <div ref='container'
+            className="thread-container">
+            <div className="thread">
+                {
+                    messages.map((mes) => {
+                        return (
+                            <div
+                                key={mes.id}
+                                className={`message-container ${mes.sender === user.name && 'right'}`}
+                            >
+                                <div className="time">{mes.time}</div>
+                                <div className="data">
+                                    <div className="message">{mes.message}</div>
+                                    <div className="name">{mes.sender}</div>
+                                </div>
+                            </div>
+
+                        )
+                    })
+                }
+                {
+                    typingUsers.map((name) => {
+                        return (
+                            <div key={name} className="typing-user">
+                                {`${name} is typing . . .`}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+
+        </div>
+    )
+}
