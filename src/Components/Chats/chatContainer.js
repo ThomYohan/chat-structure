0ee91ca6100d2b78ba0fb71989a54968fee7dff5
Thomas Berhe Ghebreyohannes <Woolu@Thomass-MacBook-Pro.local>
2019-05-16 15:53:30 -0700

    working through messages, need to make sure this is functional using react hooks

--- src/Components/Chats/chatContainer.js
+++ src/Components/Chats/chatContainer.js
@@ -1,3 +1,127 @@
-import React, { Component } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import SideBar from './SIdeBar'
-import { COMMUNITY_CHAT, MESSAGE_SENT, MESSAGE_RECEIVED, TYPING } from 
+import { COMMUNITY_CHAT, MESSAGE_SENT, MESSAGE_RECEIVED, TYPING } from '../../Events'
+import ChatHeading from './ChatHeading'
+import Messages from '../messages/Messages'
+import MessageInput from '../Messages/MessageInput'
+import { SSL_OP_MSIE_SSLV2_RSA_PADDING } from 'constants';
+
+
+const ChatContainer = (props) => {
+
+    const { user, logout } = props
+    const [activeChats, activeChat] = useState({activeChats})
+
+    const [chats, setChats] = useState([])
+
+    useEffect(() => {
+        // const { socket } = props
+        props.socket.emit(COMMUNITY_CHAT, this.resetChat)
+    })
+
+    resetChat = (chat) => {
+        return this.addChat(chat, true)
+    }
+
+    addChat = (chat, reset) => {
+        // const { socket } = this.props
+        // const { chats } = useState([])
+
+        const newChats = reset ? [chat] : [...chats, chat]
+        setChats({ chats: newChats, activeChat: reset ? chat : useState(activeChat) })
+
+        const messageEvent = `${MESSAGE_RECEIVED}-${chat.id}`
+        const typingEvent = `${TYPING}-${chat.id}`
+
+        props.socket.on(typingEvent, updateTypingInChat(chat.id))
+        props.socket.on(messageEvent, addMessageToChat(chat.id))
+    }
+
+    addMessageToChat = (chatId) => {
+        return message => {
+            // const { chats}
+            let newChats = chats.map((chat) => {
+                if (chat.id === chatId)
+                    chat.messages.push(message)
+                return chat
+            })
+
+            useState({ chats: newChats })
+        }
+    }
+
+    updateTypingInChat = (chatId) => {
+        return ({ isTyping, user }) => {
+            if (user !== props.user.name) {
+
+                let newChats = chats.map((chat) => {
+                    if (chat.id === chatID) {
+                        if (isTyping && !chat.typingUsers.includes(user)) {
+                            chat.typingUsers.push(user)
+                        } else if (!isTyping && chat.typingUsers.includes(user)) {
+                            chat.typingUsers = chat.typingUsers.filter(u => u !== user)
+                        }
+                    }
+                    return chat
+                })
+                useState({chat:newChats})
+            }
+        }
+    }
+
+
+    sendMessage = (chatId, message) => {
+        props.socket.emit(MESSAGE_SENT, {chatId, message} )
+    }
+
+    sendTyping = (chatId, isTyping) => {
+        props.socket.emit(TYPING, {chatId, isTyping})
+    }
+
+    setActiveChat = (activeChat) => {
+        useState({activeChat})
+    }
+
+
+    return (
+        <>
+        <SideBar
+            logout={logout}
+            chats={chats}
+            user={user}
+            activeChat={activeChat}
+            setActiveChat={setActiveChat}
+        />
+        <>
+        {
+            activeChat !== null ? (
+
+                <div>
+                    <ChatHeading name={activeChat.name} />
+                    <Messages 
+                        messages={activeChat.messages}
+                        user={user}
+                        typingUser={activeChat.typingUser}
+                    />
+                    <MessageInput
+                        sendMessage={
+                            (message)=>{
+                                sendMessage(activeChat.id, message)
+                            }
+                        }
+                        sendTyping={
+                            (isTyping)=>{
+                                sendTyping(activeChat.id, isTyping)
+                            }
+                        }
+                    />
+                </div>
+            ) : 
+            <div>
+                <h3>Choose a chat!</h3>
+            </div>
+        }
+        </>
+        </>
+    )
+}
---
+++ src/Components/Messages/MessageInput.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const MessageInput = (props) => {
+
+    const [message, setMessage] = useState()
+
+    const [isTyping, setIsTyping] = useState(false)
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        sendMessage()
+        useState([message = ""])
+    }
+
+    sendMessage = () => {
+        props.sendMessage(setMessage)
+    }
+
+    useEffect(() => {
+        stopCheckingTyping()
+    }, [])
+
+    sendTyping = () => {
+        lastUpdateTime = Date.now()
+        if(!setIsTyping) {
+            setIsTyping(isTyping = true)
+            props.sendTyping(true)
+            startCheckingTyping()
+        }
+    }
+
+    startCheckingTyping = () => {
+        typingInterval = setInterval(() => {
+            if((Date.now() - lastUpdateTime) > 300) {
+                setIsTyping(isTyping = false)
+                stopCheckingTyping()
+            }
+        })
+    }
+
+    stopCheckingTyping = () => {
+        if(typingInterval) {
+            clearInterval(typingInterval)
+            props.sendTyping(false)
+        }
+    }
+}
